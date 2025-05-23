01ca14063ef3c974c5cf4e693f77184de72c50e3
Urvashi Kharecha <urvashi.k@7span.com>
2025-05-23 16:01:08 +0530

    Feat version update (#1)
    * Refactor: Add colorful circular progress indicator and update dependencies
    
    This commit introduces a new `ColorfulCircularProgressIndicator` widget, which displays a circular progress indicator with customizable colors.
    
    The existing `GradientProgressIndicator` has been moved to a separate file and its behavior in the example app has been updated to showcase an auto-incrementing/decrementing progress.
    
    Dependencies have been updated, including Flutter SDK, Flutter lints, and Android Gradle plugin versions. The Android project structure has also been updated to align with newer Gradle versions.
    
    Additionally, the LICENSE has been updated to MIT.
    
    * Update README.md
    
    * Update README.md
    
    * Added video
    
    * Update README.md
    
    * Update README.md
    
    * Update changelog and version
    
    * Update README.md
    
    * Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 1.0.5
+
+* Update flutter version to 3.7.0
+* Update README.md
+* Update CHANGELOG.md
+* Add example for gradient circular progress indicator
+
 ## 1.0.4
 
-* TODO: Describe initial release.
+* Initial version of the library 
+
--- test/gradient_progress_bar_test.dart
+++ test/gradient_progress_bar_test.dart
@@ -1,11 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gradient_progress_bar/gradient_progress_bar.dart';
-
 void main() {
-  test('test', () {
-    const gradientProgressIndicator =
-        GradientProgressIndicator([Colors.black], 3);
-  });
+  test('test', () {});
 }
